b1bf6cb86275db513ce9120a3ba59ed786593310
Ehsan Mirzarazi <mirzarazi@gmail.com>
2023-08-17 19:34:03 +0200

    feat: municipality model defined

---
+++ models/municipality.ts
@@ -0,0 +1,30 @@
+import {type Association, type CreationOptional, DataTypes, type InferAttributes, type InferCreationAttributes, Model, type NonAttribute} from 'sequelize';
+import {sequelizeConnection} from '../db/config';
+import {Price} from './price';
+
+class Municipality extends Model<InferAttributes<Municipality>, InferCreationAttributes<Municipality>> {
+	declare static associations: {
+		prices: Association<Municipality, Price>;
+	};
+
+	declare id: CreationOptional<number>;
+	declare name: string;
+}
+
+Municipality.init({
+	id: {
+		type: DataTypes.INTEGER.UNSIGNED,
+		autoIncrement: true,
+		primaryKey: true,
+	},
+	name: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		unique: true,
+	},
+}, {
+	sequelize: sequelizeConnection,
+});
+
+
+export {Municipality};
