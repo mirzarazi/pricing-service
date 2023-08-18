4e451118cb1057eba0f3112d93c50ae643263441
Ehsan Mirzarazi <mirzarazi@gmail.com>
2023-08-18 12:22:36 +0200

    feat: findMunicpalityByName added

--- services/package.service.ts
+++ services/package.service.ts
@@ -10,7 +10,7 @@ export default {
 			],
 		});
   },
-  async updatePackagePrice(pack: Package, newPriceCents: number) {
+  async updatePackagePrice(pack: Package, newPriceCents: number, municipalityName: string) {
     try {
       const newPackage = await sequelizeConnection.transaction(async t => {
         await Price.create({
