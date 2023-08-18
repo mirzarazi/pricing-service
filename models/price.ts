9125942d14678f249fa85e0e7db1e85f36ccbf92
Ehsan Mirzarazi <mirzarazi@gmail.com>
2023-08-18 12:19:02 +0200

    feat: municipality id added to price model, vs code updated

--- models/price.ts
+++ models/price.ts
@@ -1,11 +1,13 @@
 import {type CreationOptional, DataTypes, type ForeignKey, type InferAttributes, type InferCreationAttributes, Model} from 'sequelize';
 import {sequelizeConnection} from '../db/config';
 import {type Package} from './package';
+import { Municipality } from './municipality';
 
 class Price extends Model<InferAttributes<Price>, InferCreationAttributes<Price>> {
 	declare id: CreationOptional<number>;
 	declare priceCents: number;
 	declare packageId: ForeignKey<Package['id']>;
+	declare municipalityId: ForeignKey<Municipality['id']>;
 
 	declare createdAt: CreationOptional<Date>;
 	declare updatedAt: CreationOptional<Date>;
